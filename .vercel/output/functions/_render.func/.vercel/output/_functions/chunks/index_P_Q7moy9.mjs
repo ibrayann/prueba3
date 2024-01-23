35f7cd3bf4e1a63bd2e74256b0723b22d74bbf41
Brayan Gatica <brayan986788@gmail.com>
2024-01-23 17:48:32 -0300

    Add new static files and update package.json and astro.config.mjs

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_P_Q7moy9.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_ZNpdx4h2.mjs');
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/pages/index_ZNpdx4h2.mjs
@@ -0,0 +1,19 @@
+import { c as createAstro, d as createComponent, r as renderTemplate, g as renderComponent, l as renderHead, e as addAttribute } from '../astro_Deh159hG.mjs';
+import 'kleur/colors';
+import { g as getCollection, $ as $$FormattedDate, a as $$BaseHead, b as $$Header, c as $$Footer, S as SITE_TITLE, d as SITE_DESCRIPTION } from './__NSNvSNSb.mjs';
+/* empty css                          */
+
+const $$Astro = createAstro("https://example.com");
+const $$Index = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Index;
+  const posts = (await getCollection("blog")).sort(
+    (a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf()
+  );
+  return renderTemplate`<html lang="en" data-astro-cid-5tznm7mj> <head>${renderComponent($$result, "BaseHead", $$BaseHead, { "title": SITE_TITLE, "description": SITE_DESCRIPTION, "data-astro-cid-5tznm7mj": true })}${renderHead()}</head> <body data-astro-cid-5tznm7mj> ${renderComponent($$result, "Header", $$Header, { "data-astro-cid-5tznm7mj": true })} <main data-astro-cid-5tznm7mj> <section data-astro-cid-5tznm7mj> <ul data-astro-cid-5tznm7mj> ${posts.map((post) => renderTemplate`<li data-astro-cid-5tznm7mj> <a${addAttribute(`/blog/${post.slug}/`, "href")} data-astro-cid-5tznm7mj> <img${addAttribute(720, "width")}${addAttribute(360, "height")}${addAttribute(post.data.heroImage, "src")} alt="" data-astro-cid-5tznm7mj> <h4 class="title" data-astro-cid-5tznm7mj>${post.data.title}</h4> <p class="date" data-astro-cid-5tznm7mj> ${renderComponent($$result, "FormattedDate", $$FormattedDate, { "date": post.data.pubDate, "data-astro-cid-5tznm7mj": true })} </p> </a> </li>`)} </ul> </section> </main> ${renderComponent($$result, "Footer", $$Footer, { "data-astro-cid-5tznm7mj": true })} </body></html>`;
+}, "/Users/brayangatica/despliegueaprod/prueba3/src/pages/blog/index.astro", void 0);
+
+const $$file = "/Users/brayangatica/despliegueaprod/prueba3/src/pages/blog/index.astro";
+const $$url = "/blog";
+
+export { $$Index as default, $$file as file, $$url as url };
